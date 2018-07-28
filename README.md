8a56de0c80303c9181be9c9bdedbf03e4d648131
Giovanny Sayas <giovanny.sayas@akendos.com>
2018-07-28 19:47:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+##Code Kata to Practice TDD in Java
+
+credits:
+1. https://www.youtube.com/watch?v=v_w7pSWeQiM
+2. http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
---
+++ src/test/java/com/example/BowlingGameTest.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import com.example.BowlingGame.BowlingGame;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BowlingGameTest {
+
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() throws Exception {
+        bowlingGame = new BowlingGame();
+
+    }
+
+    @Test
+    public void testGutterGame() {
+        rollMany(20, 0);
+        assertEquals(0, bowlingGame.score());
+
+    }
+
+    @Test
+    public void testAllOnes() {
+        rollMany(20, 1);
+        assertEquals(20, bowlingGame.score());
+
+    }
+
+    @Test
+    public void testOneSpare() {
+        rollSpare();
+        bowlingGame.roll(3);
+        rollMany(17, 0);
+    }
+
+    @Test
+    public void testOneStrike() {
+        rollStrike();
+        bowlingGame.roll(3);
+        bowlingGame.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, bowlingGame.score());
+    }
+
+    @Test
+    public void testPerfectGame() {
+        rollMany(12, 10);
+        assertEquals(300, bowlingGame.score());
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            bowlingGame.roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+    }
+
+    private void rollStrike() {
+        bowlingGame.roll(10);
+    }
+}
